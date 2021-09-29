3424d4c3e0be8f261583516ed0b81b44cd013ceb
tainguyen2019 <tainguyen.uit@gmail.com>
2021-09-29 16:52:28 +0700

    feat: add responsive to navigation

--- src/components/navigation/styles.ts
+++ src/components/navigation/styles.ts
@@ -1,15 +1,22 @@
+import { Theme } from '@mui/material';
 import { makeStyles, createStyles } from '@mui/styles';
 
-const styles = createStyles({
-  appName: {
-    flex: 1,
-  },
-  appLinks: {
-    display: 'flex',
-    flex: 1,
-    justifyContent: 'space-between',
-  },
-});
+const styles = ({ breakpoints }: Theme) =>
+  createStyles({
+    appName: {
+      [breakpoints.up('md')]: {
+        flex: 1,
+      },
+      [breakpoints.down('md')]: {
+        display: 'none',
+      },
+    },
+    appLinks: {
+      display: 'flex',
+      flex: 1,
+      justifyContent: 'space-between',
+    },
+  });
 
 const useStyles = makeStyles(styles, { classNamePrefix: 'Nav' });
 
